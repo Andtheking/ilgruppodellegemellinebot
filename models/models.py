ab5721d8c5e52bd22ea1eb7945fbb9a301d17efc
Andrea Maria Castronovo <andreamcastronovo06@gmail.com>
2026-08-19 20:41:11 +0200

    feat: migrations for DB

--- models/models.py
+++ models/models.py
@@ -1,7 +1,13 @@
-from peewee import *
+from peewee import SqliteDatabase, Model, IntegerField, TextField, BooleanField
+from peewee_migrate import Router
 
 # Connettiamo al database SQLite
 db = SqliteDatabase('secret/Database.db')
+router = Router(db, migrate_dir='migrations')
+
+def init_db():
+    """_Initialize database and apply migrations_"""
+    router.run()
 
 class BaseModel(Model):
     class Meta:
@@ -15,7 +21,3 @@ class Utente(BaseModel):
 class Chat(BaseModel):
     id = IntegerField(primary_key=True)
     title = TextField(null=True)
-
-if __name__ == '__main__':
-    db.drop_tables([Utente, Chat])
-    db.create_tables([Utente, Chat])
